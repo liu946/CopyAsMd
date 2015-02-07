a490ea2af123ebb1f6a2be4b25ecfdc5f159b487
liu946 <liuyang570926881@gmail.com>
2015-02-08 02:21:59 +0800

    Internal paste . It works in base type.

--- eventPage.js
+++ eventPage.js
@@ -1,20 +1,63 @@
 // EventPages.js
+// author : Micheal Liu
+// date : 2015年02月08日
+// markdown复制插件 · 右键菜单
+// - 智能切换复制、粘贴
+// - 累加复制（粘贴之后清空，因为存在ctrl-c原始方法）
+
+// 传出函数
+// 用于粘贴项目
+// function grin(tag) { 
+//         var myField; 
+//         myField = document.activeElement;
+//         if (document.selection) { 
+//                 myField.focus(); 
+//                 sel = document.selection.createRange(); 
+//                 sel.text = tag; 
+//                 myField.focus(); 
+//         } 
+//         else if (myField.selectionStart || myField.selectionStart == '0') { 
+//                 var startPos = myField.selectionStart; 
+//                 var endPos = myField.selectionEnd; 
+//                 var cursorPos = endPos; 
+//                 myField.value = myField.value.substring(0, startPos) 
+//                                           + tag 
+//                                           + myField.value.substring(endPos, myField.value.length); 
+//                 cursorPos += tag.length; 
+//                 myField.focus(); 
+//                 myField.selectionStart = cursorPos; 
+//                 myField.selectionEnd = cursorPos; 
+//         } 
+//         else { 
+//                 myField.value += tag; 
+//                 myField.focus(); 
+//         } 
+// } 
+//grin("'+MdClipBoard+'");'
+//
+//全局变量
+MdClipBoard='';
 chrome.contextMenus.create({
-	"title": "复制为Markdown",
+	"title": "复制",
 	id : '1',
-	"contexts": ["page", "frame", "editable", "image", "link"],
+	"contexts": ["page", "frame", "image", "link"],
 	"onclick": function handler(info, tab) {
 		if (info.mediaType=='image') {
 			str='![图片]('+info.srcUrl+')';
 		}else{
 			str='['+tab.title+']('+tab.url+')';
 		}
-		if (isChrome()) {
-			alert('请复制 '+str);
-			copyToClipboard(str);
-			window.copy("this is a test string");
-		}else{
-			window.clipboardData.setData('text',str);
-		}
+		MdClipBoard+=str;
+	}
+});
+chrome.contextMenus.create({
+	"title": "粘贴为Markdown",
+	id : '2',
+	"contexts": ["editable"],
+	"onclick": function handler(info, tab) {
+		chrome.tabs.executeScript({
+			code: 'function grin(tag) { var myField; myField = document.activeElement; if (document.selection) {  myField.focus();  sel = document.selection.createRange();  sel.text = tag;  myField.focus();  } else if (myField.selectionStart || myField.selectionStart == "0") {  var startPos = myField.selectionStart;  var endPos = myField.selectionEnd;  var cursorPos = endPos;  myField.value = myField.value.substring(0, startPos)  + tag  + myField.value.substring(endPos, myField.value.length);  cursorPos += tag.length;  myField.focus();  myField.selectionStart = cursorPos;  myField.selectionEnd = cursorPos;  }  else {  myField.value += tag;  myField.focus();  } }; grin("'+MdClipBoard+'");'
+		});
+		MdClipBoard='';
 	}
 });
